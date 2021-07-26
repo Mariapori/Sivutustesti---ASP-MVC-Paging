a92335db92039138bc40c874e3e750874311d862
Topias Mariapori <topias@mariapori.fi>
2021-07-26 13:31:32 +0300

    Add project files.

---
+++ Sivutustesti/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PagedList;
+using Sivutustesti.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sivutustesti.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(int? sivu = 1)
+        {
+            List<Rivi> rivit = new List<Rivi>();
+
+            for (int i = 0; i < 50; i++)
+            {
+                rivit.Add(new Rivi { ID = i, Esine = "Esine " + i.ToString() });
+            }
+
+
+            return View(rivit.ToPagedList(sivu.GetValueOrDefault(), 5));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Sivutustesti/Models/Rivi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sivutustesti.Models
+{
+    public class Rivi
+    {
+        public int ID { get; set; }
+        public string Esine { get; set; }
+    }
+}
